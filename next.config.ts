afe12e48bd8fdc09ebf6a4052997d0067726fa3c
Md.Nuruzaman Milon <32028951+nuruzaman-milon@users.noreply.github.com>
2024-12-24 13:46:03 +0600

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https", // Allow any protocol
+        hostname: "*", // Allow any hostname
+      },
+      {
+        protocol: "http", // Allow any protocol
+        hostname: "*", // Allow any hostname
+      },
+      {
+        protocol: "https",
+        hostname: "cryptologos.cc",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
