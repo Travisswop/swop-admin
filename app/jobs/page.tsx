5e5b47af53140bb5d28225e42144117463573c25
uiabu <ui.abukawsar@gmail.com>
2024-12-24 16:19:50 +0600

    jobs post page

---
+++ app/jobs/page.tsx
@@ -0,0 +1,9 @@
+import JobsPost from "@/components/jobs/JobsPost";
+
+export default function Home() {
+  return (
+    <section className="w-full h-full text-black">
+      <JobsPost />
+    </section>
+  );
+}
---
+++ components/jobs/JobsPost.tsx
@@ -0,0 +1,220 @@
+import Link from "next/link";
+import React from "react";
+
+const JobsPost = () => {
+  return (
+    <div className="p-10 bg-white min-h-fit h-full">
+      <div>
+        <div className="flex items-end justify-between pb-6 border-b">
+          <div className="w-full">
+            <div className=" flex items-center gap-3 ">
+              <h4 className="text-4xl font-semibold text-[#2D3032]">
+                Senior Developer
+              </h4>
+              <span className="text-base">(Published)</span>
+            </div>
+            <p className="text-lg text-[#737791] font-normal mt-2 mb-1">
+              Deadline: 5 - 8 - 2023
+            </p>
+            <div className="flex gap-3 items-center font-medium ">
+              <p className="text-lg">Full-Time</p>
+              <p className="text-lg">Salary: 15000-2000</p>
+            </div>
+          </div>
+          <div className="flex gap-5 shrink-0 ">
+            <Link
+              href="#"
+              className=" text-base font-medium py-2 flex justify-center items-center w-32   text-center border border-black rounded relative"
+            >
+              View CV
+              <span className=" w-8 h-8 bg-[#FF6060] text-white text-base font-medium rounded-full flex items-center justify-center absolute -top-2.5 -right-2.5">
+                15
+              </span>
+            </Link>
+            <Link
+              href="#"
+              className=" text-base font-medium py-2 flex justify-center items-center w-44  text-center border border-black rounded bg-black text-white"
+            >
+              Edit
+            </Link>
+          </div>
+        </div>
+        <div className="flex items-end justify-between py-6 border-b">
+          <div className="w-full">
+            <div className=" flex items-center gap-3 ">
+              <h4 className="text-4xl font-semibold text-[#2D3032]">
+                Senior Developer
+              </h4>
+              <span className="text-base">(Published)</span>
+            </div>
+            <p className="text-lg text-[#737791] font-normal mt-2 mb-1">
+              Deadline: 5 - 8 - 2023
+            </p>
+            <div className="flex gap-3 items-center font-medium ">
+              <p className="text-lg">Full-Time</p>
+              <p className="text-lg">Salary: 15000-2000</p>
+            </div>
+          </div>
+          <div className="flex gap-5 shrink-0 ">
+            <Link
+              href="#"
+              className=" text-base font-medium py-2 flex justify-center items-center w-32   text-center border border-black rounded relative"
+            >
+              View CV
+              <span className=" w-8 h-8 bg-[#FF6060] text-white text-base font-medium rounded-full flex items-center justify-center absolute -top-2.5 -right-2.5">
+                15
+              </span>
+            </Link>
+            <Link
+              href="#"
+              className=" text-base font-medium py-2 flex justify-center items-center w-44  text-center border border-black rounded bg-black text-white"
+            >
+              Edit
+            </Link>
+          </div>
+        </div>
+        <div className="flex items-end justify-between py-6 border-b">
+          <div className="w-full">
+            <div className=" flex items-center gap-3 ">
+              <h4 className="text-4xl font-semibold text-[#2D3032]">
+                Senior Developer
+              </h4>
+              <span className="text-base">(Published)</span>
+            </div>
+            <p className="text-lg text-[#737791] font-normal mt-2 mb-1">
+              Deadline: 5 - 8 - 2023
+            </p>
+            <div className="flex gap-3 items-center font-medium ">
+              <p className="text-lg">Full-Time</p>
+              <p className="text-lg">Salary: 15000-2000</p>
+            </div>
+          </div>
+          <div className="flex gap-5 shrink-0 ">
+            <Link
+              href="#"
+              className=" text-base font-medium py-2 flex justify-center items-center w-32   text-center border border-black rounded relative"
+            >
+              View CV
+              <span className=" w-8 h-8 bg-[#FF6060] text-white text-base font-medium rounded-full flex items-center justify-center absolute -top-2.5 -right-2.5">
+                15
+              </span>
+            </Link>
+            <Link
+              href="#"
+              className=" text-base font-medium py-2 flex justify-center items-center w-44  text-center border border-black rounded bg-black text-white"
+            >
+              Edit
+            </Link>
+          </div>
+        </div>
+        <div className="flex items-end justify-between py-6 border-b">
+          <div className="w-full">
+            <div className=" flex items-center gap-3 ">
+              <h4 className="text-4xl font-semibold text-[#2D3032]">
+                Senior Developer
+              </h4>
+              <span className="text-base">(Published)</span>
+            </div>
+            <p className="text-lg text-[#737791] font-normal mt-2 mb-1">
+              Deadline: 5 - 8 - 2023
+            </p>
+            <div className="flex gap-3 items-center font-medium ">
+              <p className="text-lg">Full-Time</p>
+              <p className="text-lg">Salary: 15000-2000</p>
+            </div>
+          </div>
+          <div className="flex gap-5 shrink-0 ">
+            <Link
+              href="#"
+              className=" text-base font-medium py-2 flex justify-center items-center w-32   text-center border border-black rounded relative"
+            >
+              View CV
+              <span className=" w-8 h-8 bg-[#FF6060] text-white text-base font-medium rounded-full flex items-center justify-center absolute -top-2.5 -right-2.5">
+                15
+              </span>
+            </Link>
+            <Link
+              href="#"
+              className=" text-base font-medium py-2 flex justify-center items-center w-44  text-center border border-black rounded bg-black text-white"
+            >
+              Edit
+            </Link>
+          </div>
+        </div>
+        <div className="flex items-end justify-between py-6 border-b">
+          <div className="w-full">
+            <div className=" flex items-center gap-3 ">
+              <h4 className="text-4xl font-semibold text-[#2D3032]">
+                Senior Developer
+              </h4>
+              <span className="text-base">(Published)</span>
+            </div>
+            <p className="text-lg text-[#737791] font-normal mt-2 mb-1">
+              Deadline: 5 - 8 - 2023
+            </p>
+            <div className="flex gap-3 items-center font-medium ">
+              <p className="text-lg">Full-Time</p>
+              <p className="text-lg">Salary: 15000-2000</p>
+            </div>
+          </div>
+          <div className="flex gap-5 shrink-0 ">
+            <Link
+              href="#"
+              className=" text-base font-medium py-2 flex justify-center items-center w-32   text-center border border-black rounded relative"
+            >
+              View CV
+              <span className=" w-8 h-8 bg-[#FF6060] text-white text-base font-medium rounded-full flex items-center justify-center absolute -top-2.5 -right-2.5">
+                15
+              </span>
+            </Link>
+            <Link
+              href="#"
+              className=" text-base font-medium py-2 flex justify-center items-center w-44  text-center border border-black rounded bg-black text-white"
+            >
+              Edit
+            </Link>
+          </div>
+        </div>
+        <div className="flex items-end justify-between pt-6 ">
+          <div className="w-full">
+            <div className=" flex items-center gap-3 ">
+              <h4 className="text-4xl font-semibold text-[#2D3032]">
+                Senior Developer
+              </h4>
+              <span className="text-base">(Published)</span>
+            </div>
+            <p className="text-lg text-[#737791] font-normal mt-2 mb-1">
+              Deadline: 5 - 8 - 2023
+            </p>
+            <div className="flex gap-3 items-center font-medium ">
+              <p className="text-lg">Full-Time</p>
+              <p className="text-lg">Salary: 15000-2000</p>
+            </div>
+          </div>
+          <div className="flex gap-5 shrink-0 ">
+            <Link
+              href="#"
+              className=" text-base font-medium py-2 flex justify-center items-center w-32   text-center border border-black rounded relative"
+            >
+              View CV
+              <span className=" w-8 h-8 bg-[#FF6060] text-white text-base font-medium rounded-full flex items-center justify-center absolute -top-2.5 -right-2.5">
+                15
+              </span>
+            </Link>
+            <Link
+              href="#"
+              className=" text-base font-medium py-2 flex justify-center items-center w-44  text-center border border-black rounded bg-black text-white"
+            >
+              Edit
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div>
+        <Link href="#">Add Job Post</Link>
+      </div>
+    </div>
+  );
+};
+
+export default JobsPost;
