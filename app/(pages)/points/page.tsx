b0b5a0830c935da9aa7e5666995f80a5ad6dc209
Md.Nuruzaman Milon <32028951+nuruzaman-milon@users.noreply.github.com>
2025-03-23 10:27:07 +0600

    update

--- app/(pages)/points/page.tsx
+++ app/(pages)/points/page.tsx
@@ -1,6 +1,8 @@
 import { getPointsLists } from "@/action/swopId";
 import PointsLists from "@/components/points/PointsLists";
+import { Button } from "@/components/ui/button";
 import { cookies } from "next/headers";
+import Link from "next/link";
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 interface PageProps {
@@ -16,7 +18,14 @@ const PointsPage = async ({ searchParams }: PageProps) => {
   return (
     <div className="w-full max-h-[90%] text-black overflow-scroll-y bg-white p-4 flex flex-col gap-5 rounded-lg py-10">
       <article className="flex justify-between items-center w-full gap-4">
-        <h2 className="text-xl 2xl:text-2xl font-semibold pl-5">User Points</h2>
+        <div className="flex items-center gap-2">
+          <h2 className="text-xl 2xl:text-2xl font-semibold pl-5">
+            User Points
+          </h2>
+          <Link href={`/points/edit/update-points`}>
+            <Button variant={"default"}>Edit Points</Button>
+          </Link>
+        </div>
         <div className="w-96 relative">
           <input
             type="text"
