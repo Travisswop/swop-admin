fe0c803890060bf1cab20ba6b04e8bf63eabf95a
arsahak <arsahak99@gmail.com>
2025-06-18 14:19:50 +0600

    Update subscripter table  design

--- components/subscribe/ToggleSwitch.tsx
+++ components/subscribe/ToggleSwitch.tsx
@@ -12,23 +12,6 @@ const ToggleSwitch: React.FC = () => {
       <span className="mr-3 text-base font-medium text-gray-900 dark:text-gray-300">
         Enterprise Leads
       </span>
-      <svg
-        className="w-6 h-6 text-gray-800 dark:text-white"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <path
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M12 19V5m0 14-4-4m4 4 4-4"
-        />
-      </svg>
 
       <input
         type="checkbox"
