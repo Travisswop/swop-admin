c57cbad6528f734e8d08dddd840c83d14f43f348
arsahak <arsahak99@gmail.com>
2025-06-23 14:38:32 +0600

    dashboard subsidy wallet data update

--- components/homepageChart/SubsidyChart.tsx
+++ components/homepageChart/SubsidyChart.tsx
@@ -34,108 +34,97 @@ const SubsidyWalletChart: React.FC = () => {
   return (
     <div className="bg-white rounded-xl shadow-lg p-6 max-w-sm flex-[2] xl:flex-[1.5] 2xl:flex-1">
       <h2 className="text-lg font-bold mb-4">Subsidy Wallet</h2>
-      <ResponsiveContainer width="100%" height={200}>
-        <BarChart data={chartData}>
-          <XAxis
-            dataKey="month"
-            axisLine={false}
-            tickLine={false}
-            tick={{ fill: "#999", fontSize: 12 }}
-          />
-          <Tooltip
-            contentStyle={{
-              backgroundColor: "#fff",
-              borderRadius: "8px",
-              border: "1px solid #ddd",
-              padding: "10px",
-            }}
-          />
-          <Bar
-            dataKey="polygon"
-            fill="#a78bfa"
-            barSize={20}
-            radius={[4, 4, 0, 0]}
-          />
-          <Bar
-            dataKey="swopple"
-            fill="#000"
-            barSize={20}
-            radius={[4, 4, 0, 0]}
-          />
-        </BarChart>
-      </ResponsiveContainer>
-      <div className="mt-6">
-        <div className="p-4 space-y-4 h-[200px] overflow-y-auto">
-          {data?.map((token, index) => {
-            const icon = token?.marketData?.iconUrl || "";
-            const price = parseFloat(token?.marketData?.price || "0");
-            const balance = token?.balance;
-            const formattedBalance = balance / 10 ** token?.decimals;
-            const usdValue = formattedBalance * price;
-
-            return (
+      {loading ? (
+        <div className="">
+          <div role="status" className="max-w-sm animate-pulse">
+            <div className="flex items-center justify-center w-full h-36 bg-gray-300 rounded-sm"></div>
+            {[1, 2, 3, 4, 5, 6].map((el, index) => (
               <div
                 key={index}
-                className="flex items-center gap-4 border-b pb-2"
+                className="flex items-center justify-between space-x-4 mt-4"
               >
-                {icon && (
-                  <Image
-                    width={24}
-                    height={24}
-                    src={icon}
-                    alt={token.symbol}
-                    className="w-6 h-6"
-                  />
-                )}
-                <div className="flex-1">
-                  <div className="font-semibold text-sm">
-                    {token.name} ({token.symbol})
-                  </div>
-                  <div className="text-xs text-gray-500">
-                    Balance: {formattedBalance.toFixed(4)} | Price: $
-                    {price.toFixed(4)}
-                  </div>
-                </div>
-                <div className="font-medium text-base text-gray-800">
-                  ${usdValue.toFixed(4)}
-                </div>
+                <div className="h-7 bg-gray-200 rounded-full dark:bg-gray-700 w-48"></div>
+                <div className="h-7 bg-gray-200 rounded-full dark:bg-gray-700 w-20"></div>
               </div>
-            );
-          })}
+            ))}
+          </div>
         </div>
-        {/* <div className="flex items-center mb-4">
-          <div className="w-8 h-8 bg-purple-200 rounded-full flex items-center justify-center">
-            <Image
-              src="https://cryptologos.cc/logos/polygon-matic-logo.png"
-              alt="Polygon"
-              className="w-4 h-4"
-              width={60}
-              height={60}
-            />
+      ) : (
+        <div className="">
+          <ResponsiveContainer width="100%" height={200}>
+            <BarChart data={chartData}>
+              <XAxis
+                dataKey="month"
+                axisLine={false}
+                tickLine={false}
+                tick={{ fill: "#999", fontSize: 12 }}
+              />
+              <Tooltip
+                contentStyle={{
+                  backgroundColor: "#fff",
+                  borderRadius: "8px",
+                  border: "1px solid #ddd",
+                  padding: "10px",
+                }}
+              />
+              <Bar
+                dataKey="polygon"
+                fill="#a78bfa"
+                barSize={20}
+                radius={[4, 4, 0, 0]}
+              />
+              <Bar
+                dataKey="swopple"
+                fill="#000"
+                barSize={20}
+                radius={[4, 4, 0, 0]}
+              />
+            </BarChart>
+          </ResponsiveContainer>
+          <div className="mt-6">
+            <div className="p-4 space-y-4 h-[200px] overflow-y-auto">
+              {data?.map((token, index) => {
+                const icon = token?.marketData?.iconUrl || "";
+                const price = parseFloat(token?.marketData?.price || "0");
+                const balance = token?.balance;
+
+                const formattedBalance = balance / 10 ** token?.decimals;
+
+                const usdValue = formattedBalance * price;
+
+                return (
+                  <div
+                    key={index}
+                    className="flex items-center gap-4 border-b pb-2"
+                  >
+                    {icon && (
+                      <Image
+                        width={24}
+                        height={24}
+                        src={icon}
+                        alt={token.symbol}
+                        className="w-6 h-6"
+                      />
+                    )}
+                    <div className="flex-1">
+                      <div className="font-semibold text-sm">
+                        {token.name} ({token.symbol})
+                      </div>
+                      <div className="text-xs text-gray-500">
+                        Balance: {formattedBalance.toFixed(8)} | Price: $
+                        {price.toFixed(4)}
+                      </div>
+                    </div>
+                    <div className="font-medium text-base text-gray-800">
+                      {Number(usdValue).toFixed(8)}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <div className="ml-3">
-            <p className="font-medium">Polygon</p>
-            <p className="text-xs text-gray-500">Polygon</p>
-          </div>
-          <p className="ml-auto font-semibold text-purple-500">$ 7000.00</p>
-        </div> */}
-        {/* <div className="flex items-center">
-          <div className="w-8 h-8 bg-gray-100 rounded-full flex items-center justify-center">
-            <Image
-              src="/images/swop-logo.png"
-              alt="Swopple"
-              className="w-5 h-auto"
-              width={150}
-              height={150}
-            />
-          </div>
-          <div className="ml-3">
-            <p className="font-medium">Swopple</p>
-            <p className="text-xs text-gray-500">Swopple</p>
-          </div>
-          <p className="ml-auto font-semibold">$ {formattedBalance}</p>
-        </div> */}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
